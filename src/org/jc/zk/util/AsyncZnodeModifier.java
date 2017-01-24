1eed14862679ecceba54873819298cc6111c356a
cespedjo <cespedjo@PCSISDWH-ASU24.personal.com.py>
2017-01-24 10:02:43 -0300

    ** Modified the MW deletion policy, by allowing the upper level TM to own permission grant. So, IMWs request permission to remove znodes and only the IMW that arrived first gets the permission to do it, and also is the next AMW. ** Once the next AMW gets permission to remove znodes, it sets the permission znode to BUSY. ** No one can request anything while busy. ** Added AsyncZnodeModifier class which is used by ATM to clear permission znode's status after 2*timeIntervalMillis millis.

---
+++ src/org/jc/zk/util/AsyncZnodeModifier.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jc.zk.Util;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooKeeper;
+/**
+ *
+ * @author cespedjo
+ */
+public class AsyncZnodeModifier implements Runnable {
+    
+    private final String znode;
+    
+    private final byte[] data;
+    
+    private final ZooKeeper zk;
+    
+    private final long modifyWaitMillis;
+    
+    private long timeStarted;
+    
+    private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(AsyncZnodeModifier.class);
+    
+    public AsyncZnodeModifier(String znode, byte[] data, ZooKeeper zk, long modifyWaitMillis) {
+        this.znode = znode;
+        this.data = data;
+        this.zk = zk;
+        this.modifyWaitMillis = modifyWaitMillis;
+    }
+    
+    @Override
+    public void run() {
+        this.timeStarted = System.currentTimeMillis();
+        synchronized (this) {
+            while (true) {
+                try {
+                    logger.info("Waiting " + this.modifyWaitMillis + " millis before restoring " + this.znode + " znode to FREE.");
+                    wait(this.modifyWaitMillis);
+                    break;
+                } catch (InterruptedException ex) {
+                    logger.error("Thread interrupted while waiting " + this.modifyWaitMillis + " millis to set " + this.znode + " to FREE.", ex);
+                    if (System.currentTimeMillis() - this.timeStarted > this.modifyWaitMillis) {
+                        logger.error("Interrupted while waiting but finished waiting and now it is time to set " + this.znode + " to FREE. Breaking.");
+                        break;
+                    }
+                }
+            }
+            try {
+                logger.info("Restoring " + this.znode + " to FREE now.");
+                this.zk.setData(this.znode, data, -1);
+            } catch (KeeperException ex) {
+                logger.error("Failed to restore " + this.znode + " to FREE.", ex);
+            } catch (InterruptedException ex) {
+                logger.error("Thread interrupted while waiting for ZK to finish setting znode to FREE.", ex);
+            }
+        }
+    }
+}
