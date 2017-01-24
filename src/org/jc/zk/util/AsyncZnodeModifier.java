6256db49402c335d0a0ef4c0b0b9f450987b8872
cespedjo <cespedjo@PCSISDWH-ASU24.personal.com.py>
2017-01-24 10:41:10 -0300

    ** Modified the MW deletion policy, by allowing the upper level TM to own permission grant. So, IMWs request permission to remove znodes and only the IMW that arrived first gets the permission to do it, and also is the next AMW. ** Once the next AMW gets permission to remove znodes, it sets the permission znode to BUSY. ** No one can request anything while busy. ** Added AsyncZnodeModifier class which is used by ATM to clear permission znode's status after 2*timeIntervalMillis millis.

--- src/org/jc/zk/util/AsyncZnodeModifier.java
+++ src/org/jc/zk/util/AsyncZnodeModifier.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.jc.zk.Util;
+package org.jc.zk.util;
 
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooKeeper;
